c87e1320167869052627d62f4d7f28a0f4d45e7c
yf.feng <yf.feng@shunwang.com>
2018-11-19 09:37:20 +0800

    cleanall

--- dictionary/commfile/convert.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-bool UTF82wchar(const std::basic_string<char> str_utf8, std::basic_string<wchar_t>& str_wchar);
-bool wchar2UTF8(const std::basic_string<wchar_t> str_wchar, std::basic_string<char>& str_utf8);
-bool wchar2char(const std::basic_string<wchar_t> str_wchar, std::basic_string<char>& str_char);
-bool char2wchar(const std::basic_string<char> str_char, std::basic_string<wchar_t>& str_wchar);
